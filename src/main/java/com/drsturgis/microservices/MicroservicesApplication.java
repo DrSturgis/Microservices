5bbe57b9cd02c0b3b6bb67e1e254950abfe3a06f
Matheu <matheu.castelo@hotmail.com>
2023-03-16 12:38:29 -0300

    First commit

---
+++ src/main/java/com/drsturgis/microservices/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.drsturgis.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+}
